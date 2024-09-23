0868147b0e0f7ab5e53acdf0e041261a70f4b9e0
pierreleripoll <pierreripoll13@gmail.com>
2024-09-23 14:15:18 +0200

    chore: add @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BASE_URL || "/",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
